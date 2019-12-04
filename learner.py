03da59cdf11a87424976f5b20cb0f31389ece9ed
richard <richard@mars.rac.local>
2019-12-03 21:19:19 -0500

    Separated models

--- learner.py
+++ learner.py
@@ -1,54 +1,45 @@
 # -*- coding: utf-8 -*-
 
-import time
 import random
-import gym
-import VehicleBall_v0
-import model 
 
+import gym
 import torch
-import torch.nn as nn
-import torch.nn.functional as F
 import torch.optim as optim
 
-
-def main( model_file, num_iterations, learning_rate ) :
-    learner = Learner() 
-
-    learner.learn( 1000 ) 
-    learner.exploit() 
+import model
+import VehicleBall_v0
 
 
-class Learner() :
-
+class Learner:
     BATCH_SIZE = 50
     NUM_STEPS = 100
     DECAY = 0.99
-    CONSECUTIVE_FRAME_COUNT=4
+    CONSECUTIVE_FRAME_COUNT = 4
     FULLY_RANDOM = 1.0
-    
 
-    def __init__( self, model_file, hidden_sizes, num_iterations, learning_rate ) :
-        self.env = gym.make( "VehicleBall-v0", render_mode='human' )
-        
+    def __init__(self, model_file, hidden_sizes, num_iterations, learning_rate):
+        self.env = gym.make("VehicleBall-v0", render_mode='human')
+
         self.num_actions = self.env.action_space.n
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
         # 4 obs + 1 action
         self.numInputs = Learner.CONSECUTIVE_FRAME_COUNT * \
-                            self.env.observation_space.shape[0] + 1   
+                         self.env.observation_space.shape[0] + 1
 
-        self.mlp = model.Model( self.numInputs, 1, hidden_sizes, model_file ).to( self.device )
-        self.optimizer = optim.SGD( self.mlp.parameters(), lr=learning_rate )
+        self.vae = model.VAE(self.numInputs, ZDIMS=5, ZHID=10).to(self.device)
+        self.optimizer_vae = optim.SGD(self.vae.parameters(), lr=learning_rate)
+
+        self.mlp = model.Model(self.numInputs, 1, hidden_sizes, model_file).to(self.device)
+        self.optimizer_mlp = optim.SGD(self.mlp.parameters(), lr=learning_rate)
 
         self.model_file = model_file
         self.num_iterations = num_iterations
-        
+
         self.clear()
 
-
-    def clear( self ) :
-        obs = self.env.reset() 
+    def clear(self):
+        obs = self.env.reset()
 
         #  Reset state of the model
         self.rewards = []
@@ -56,71 +47,65 @@ class Learner() :
         self.actions = []
         self.dones = []
 
-        for _ in range( self.CONSECUTIVE_FRAME_COUNT ) :
-            self.observations.append( obs )
+        for _ in range(self.CONSECUTIVE_FRAME_COUNT):
+            self.observations.append(obs)
 
-
-
-    def learn( self ) :
+    def learn(self):
         self.clear()
 
-        for it in range( self.num_iterations ) :
-            x, y, actions = self.collect( self.FULLY_RANDOM, self.BATCH_SIZE )
+        for it in range(self.num_iterations):
+            x, y, actions = self.collect(self.FULLY_RANDOM, self.BATCH_SIZE)
 
-            if len( x ) == 0 :
+            if len(x) == 0:
                 break
-            for i in range( len(x) ) :
-                x[i].append( float(actions[i]) )
+            for i in range(len(x)):
+                x[i].append(float(actions[i]))
 
-            x = torch.tensor( x, device=self.device )
-            y = torch.tensor( y, device=self.device ).unsqueeze(1)
-            loss = self.applyLearning( x, y )
+            x = torch.tensor(x, device=self.device)
+            y = torch.tensor(y, device=self.device).unsqueeze(1)
+            loss = self.apply_learning(x, y)
 
-            print( it, loss )
+            print(it, loss)
 
+    def step(self, random_chance, num_steps=1):
 
+        num_needed = num_steps + self.NUM_STEPS - len(self.actions)
 
-    def step( self, random_chance, num_steps=1 ) :
-
-        num_needed = num_steps + self.NUM_STEPS - len( self.actions )
-
-        for start in range( num_needed ) :
-            action = self.decide_action( start, random_chance ) 
-            obs, reward, done, _ = self.env.step( action ) 
+        for start in range(num_needed):
+            action = self.decide_action(start, random_chance)
+            obs, reward, done, _ = self.env.step(action)
 
             # for BCE loss we need inputs normalized 0 .. 1
-            obs = list( map( lambda o : o + 1.0 / 2.0, obs ) )
+            obs = list(map(lambda o: o + 1.0 / 2.0, obs))
 
             # previous_obs | action => obs | reward
-            self.observations.append( obs ) 
-            self.rewards.append( reward ) 
-            self.actions.append( action )
-            self.dones.append( done )
+            self.observations.append(obs)
+            self.rewards.append(reward)
+            self.actions.append(action)
+            self.dones.append(done)
 
+    def collect(self, random_chance, num_steps=1):
+        self.step(random_chance, num_steps)
 
-
-    def collect( self, random_chance, num_steps=1 ) :
-        self.step( random_chance, num_steps )
-
-        values = [] 
-        for ix in range( num_steps ) :
+        values = []
+        for ix in range(num_steps):
             # discount rewards from resulting state N steps in the future
-            v = self.discountRewards( self.rewards, ix, self.NUM_STEPS )
-            values.append( v ) 
+            v = self.discount_rewards(self.rewards, ix, self.NUM_STEPS)
+            values.append(v)
 
         inputs = []
-        for i in range( num_steps) :
-            o = [] 
-            for j in range( i, i+self.CONSECUTIVE_FRAME_COUNT ) :
-                o.extend( self.observations[j] )
-            inputs.append( o )
+        for i in range(num_steps):
+            o = []
+            for j in range(i, i + self.CONSECUTIVE_FRAME_COUNT):
+                o.extend(self.observations[j])
+            inputs.append(o)
 
-        actions = self.actions[ 0:num_steps ]
+        actions = self.actions[0:num_steps]
 
-        try :
-            n = self.dones.index( True )
-        except ValueError :
-            n = num_steps 
+        try:
+            n = self.dones.index(True)
+        except ValueError:
+            n = num_steps
 
         del self.observations[0:num_steps]
         del self.rewards[0:num_steps]
@@ -129,98 +114,94 @@ class Learner() :
 
         return inputs[0:n], values[0:n], actions[0:n]
 
-
-
-    def decide_action( self, start, random_chance=FULLY_RANDOM ) :
-        if random.random() < random_chance :
-            return self.env.action_space.sample() 
+    def decide_action(self, start, random_chance=FULLY_RANDOM):
+        if random.random() < random_chance:
+            return self.env.action_space.sample()
 
         frames = []
-        for i in range( self.CONSECUTIVE_FRAME_COUNT ) :
-            frames.extend( self.observations[i + start] )
+        for i in range(self.CONSECUTIVE_FRAME_COUNT):
+            frames.extend(self.observations[i + start])
 
         best_action = 0
-        with torch.no_grad() :
-            state = torch.tensor( frames + [ 0.0 ], dtype=torch.float,device=self.device )
-            yhat, recon_x, mu, logvar = self.mlp( state )
-            best_value = yhat.item() 
-            
-            for action in range( 1, self.num_actions ) :
-                state[ len(frames) ] = float(action)
-                yhat, _, _, _ = self.mlp( state )
-                state_value = yhat.item()
+        with torch.no_grad():
+            state = torch.tensor(frames + [0.0], dtype=torch.float, device=self.device)
+            y_hat = self.mlp(state)
+            best_value = y_hat.item()
 
-                if state_value > best_value :
+            for action in range(1, self.num_actions):
+                state[len(frames)] = float(action)
+                y_hat = self.mlp(state)
+                state_value = y_hat.item()
+
+                if state_value > best_value:
                     best_action = action
                     best_value = state_value
 
         return best_action
 
+    def exploit(self, iterations=-1):
 
-    def exploit(self, iterations=-1) :
-        
-        random_chance = self.FULLY_RANDOM 
+        random_chance = self.FULLY_RANDOM
         always = iterations == -1
         it = 0
-        while always or iterations>0 :
+        while always or iterations > 0:
             it = it + 1
             # with torch.no_grad() :
             self.clear()
             random_chance = random_chance - 0.05
-            if random_chance < 0.03 :
+            if random_chance < 0.03:
                 random_chance = 0.03
 
-            input_history = [] 
+            input_history = []
             value_history = []
-            for stp in range(5000) :
-                inputs, values, actions = self.collect( random_chance )
-                if len(inputs) == 0 :
-                    break # we're done !
+            for stp in range(5000):
+                inputs, values, actions = self.collect(random_chance)
+                if len(inputs) == 0:
+                    break  # we're done !
 
-                inputs[0].append( float(actions[0]) )
+                inputs[0].append(float(actions[0]))
                 # Add new observation to end & 'forget' the oldest observation 
-                input_history.extend( inputs )
-                value_history.extend( values )
+                input_history.extend(inputs)
+                value_history.extend(values)
 
-                if len(input_history) > self.BATCH_SIZE :
-                    inputs = torch.tensor( input_history, device=self.device )
-                    values = torch.tensor( value_history, device=self.device ).unsqueeze(1)
-                    loss = self.applyLearning( inputs, values )
-                    print( it, loss, random_chance )
-                    input_history = [] 
+                if len(input_history) > self.BATCH_SIZE:
+                    inputs = torch.tensor(input_history, device=self.device)
+                    values = torch.tensor(value_history, device=self.device).unsqueeze(1)
+                    loss_mlp, loss_vae = self.apply_learning(inputs, values)
+                    print(it, loss_mlp, loss_vae, random_chance)
+                    input_history = []
                     value_history = []
 
-                if ( stp & 63 ) == 0 :
-                    self.photo() 
+                if (stp & 63) == 0:
+                    self.photo()
 
             iterations = iterations - (0 if always else 1)
             self.mlp.save()
 
+    def apply_learning(self, x, y):
 
+        recon_x, mu, log_var = self.vae(x)
+        loss_vae = self.vae.loss(recon_x, x, mu, log_var)
+        self.optimizer_vae.zero_grad()  # zero the gradient buffers
+        loss_vae.backward()  # calc gradients
+        self.optimizer_vae.step()  # update weights
 
-    def applyLearning( self, x, y ) :
+        y_hat = self.mlp(x)
+        loss_mlp = self.mlp.loss(y, y_hat)
+        self.optimizer_mlp.zero_grad()  # zero the gradient buffers
+        loss_mlp.backward()  # calc gradients
+        self.optimizer_mlp.step()  # update weights
 
-        yhat, recon_x, mu, logvar = self.mlp( x )
+        return loss_mlp.item(), loss_vae.item()
 
-        loss = self.mlp.loss( yhat, y, recon_x, x, mu, logvar )
+    def photo(self):
+        self.env.render()
 
-        self.optimizer.zero_grad()  # zero the gradient buffers
-        loss.backward()             # calc gradients
-        self.optimizer.step()       # update weights
-
-        return loss.item()
-
-
-    def photo( self ) :
-        self.env.render() 
-
-
-    def discountRewards( self, rewards, offset, length, gamma=0.99 ) :
-        value = 0 
-        for ix in range( offset+length, offset, -1 ):
-            if self.dones[ix] :
-                value = 0 
+    def discount_rewards(self, rewards, offset, length, gamma=0.99):
+        value = 0
+        for ix in range(offset + length, offset, -1):
+            if self.dones[ix]:
+                value = 0
             value = value * gamma + rewards[ix]
 
-        return value 
- 
+        return value
--- model.py
+++ model.py
@@ -4,82 +4,99 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-ZDIMS = 5
-ZHID = 10
+
 class Model(nn.ModuleDict):
 
-    def __init__(self, numInputs, numOutputs, hidden_sizes, model_file ):
+    def __init__(self, num_inputs, num_outputs, hidden_sizes, model_file):
         super(Model, self).__init__()
-
-        # VAE section
-        self.encoder = nn.Sequential( 
-                nn.Linear(numInputs, ZHID ),
-                nn.ReLU( True )
-        )
-        self.encoderMu = nn.Linear(ZHID, ZDIMS)  # mu layer
-        self.encoderLogVar = nn.Linear(ZHID, ZDIMS)  # logvariance layer
-        # VAE bottleneck
-
-        self.decoder = nn.Sequential( 
-            nn.Linear(ZDIMS, ZHID),
-            nn.ReLU( True ),
-            nn.Linear(ZHID, numInputs ),
-            nn.Sigmoid()
-        )
 
         # MLP section
         layers = []
-        ix = 0 
-        prev_layer = numInputs 
-        for h in hidden_sizes :
-            layers.append( nn.Linear( prev_layer, h ) ) 
-            layers.append( nn.ReLU( True ) ) 
+        ix = 0
+        prev_layer = num_inputs
+        for h in hidden_sizes:
+            layers.append(nn.Linear(prev_layer, h))
+            layers.append(nn.ReLU(True))
             prev_layer = h
 
-        layers.append( nn.Linear( h, numOutputs ) ) 
-        self.seq = nn.Sequential( *layers )
+        layers.append(nn.Linear(h, num_outputs))
+        self.seq = nn.Sequential(*layers)
         self.model_file = model_file
         self.criterion = nn.MSELoss()
 
-
-    def encode( self, x ) :
-        h1 = self.encoder(x)
-        mu = self.encoderMu(h1)
-        logvar = self.encoderLogVar(h1)
-        return mu, logvar
-    
-    def reparameterize( self, mu, logvar ) :
-        std = torch.exp( logvar * 0.5 )  # type: torch.tensor
-        eps = torch.randn_like( std )
-        return eps * std + mu 
-
-    def decode(self, z ):
-        return self.decoder( z )
-
     def forward(self, x):
-        # first pass through VAE
-        mu, logvar = self.encode( x )
-        mu2 = self.reparameterize(mu, logvar)
-        recon_x = self.decode(mu2)
-        # Then MLP to get categorization
-        yhat = self.seq( recon_x )
-        return yhat, recon_x, mu, logvar
+        y_hat = self.seq(x)
+        return y_hat
 
+    def loss(self, y, y_hat):
+        return self.criterion(y, y_hat)
 
-    def loss( self, yhat, y, recon_x, x, mu, logvar ) :
-        BCE = F.binary_cross_entropy(recon_x, x, reduction='sum')
-        KLD = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
-        # BCE tries to make our reconstruction as accurate as possible
-        # KLD tries to push the distributions as close as possible to unit Gaussian
-        return BCE + KLD + self.criterion( y, yhat )
-
-
-    def save( self ) :
-        torch.save( self.state_dict(), self.model_file )
+    def save(self):
+        torch.save(self.state_dict(), self.model_file)
 
     @staticmethod
-    def load( file="model.pt" ) :
-        this = Model( model_file=file ) 
-        this.load_state_dict( torch.load(file) )
+    def load(file="model.pt"):
+        this = Model(model_file=file)
+        this.load_state_dict(torch.load(file))
         return this
 
+
+class VAE(nn.ModuleDict):
+
+    def __init__(self, num_inputs, ZDIMS=5, ZHID=10, model_file="vae.pt"):
+        super(VAE, self).__init__()
+
+        # VAE section
+        self.encoder = nn.Sequential(
+            nn.Linear(num_inputs, ZHID),
+            nn.ReLU(True)
+        )
+        self.encoderMu = nn.Linear(ZHID, ZDIMS)
+        self.encoderLogVar = nn.Linear(ZHID, ZDIMS)
+        # VAE bottleneck
+
+        self.decoder = nn.Sequential(
+            nn.Linear(ZDIMS, ZHID),
+            nn.ReLU(True),
+            nn.Linear(ZHID, num_inputs),
+            nn.Sigmoid()
+        )
+
+        self.model_file = model_file
+
+    def encode(self, x):
+        h1 = self.encoder(x)
+        mu = self.encoderMu(h1)
+        log_var = self.encoderLogVar(h1)
+        return mu, log_var
+
+    def reparameterize(self, mu, log_var):
+        std = torch.exp(log_var * 0.5)
+        eps = torch.randn_like(std)
+        return eps * std + mu
+
+    def decode(self, z):
+        return self.decoder(z)
+
+    def forward(self, x):
+        # pass through VAE
+        mu, log_var = self.encode(x)
+        mu2 = self.reparameterize(mu, log_var)
+        recon_x = self.decode(mu2)
+        return recon_x, mu, log_var
+
+    def loss(self, recon_x, x, mu, log_var):
+        BCE = F.binary_cross_entropy(recon_x, x, reduction='sum')
+        KLD = -0.5 * torch.sum(1 + log_var - mu.pow(2) - log_var.exp())
+        # BCE tries to make our reconstruction as accurate as possible
+        # KLD tries to push the distributions as close as possible to unit Gaussian
+        return BCE + KLD
+
+    def save(self):
+        torch.save(self.state_dict(), self.model_file)
+
+    @staticmethod
+    def load(file="vae.pt"):
+        this = Model(model_file=file)
+        this.load_state_dict(torch.load(file))
+        return this
