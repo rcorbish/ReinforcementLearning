4ace6bfd0f8054d2c679fb35162cd5388a190cb1
rcorbish <rcorbish@outlook.com>
2019-12-05 07:15:18 -0500

    Commented out VAE

--- model.py
+++ model.py
@@ -5,38 +5,85 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 
-class Model(nn.ModuleDict):
-
+class Model :
     def __init__(self, num_inputs, num_outputs, hidden_sizes, model_file_prefix):
-        super(Model, self).__init__()
+        self.mlp = MLP( num_inputs, num_outputs, hidden_sizes, model_file_prefix )
+        self.vae = MLP( num_inputs, num_outputs, hidden_sizes, model_file_prefix )
 
-        # MLP section
-        layers = []
+    def forward(self, x):
+        y_hat = self.mlp(x)
+        return y_hat
+
+    def loss(self, y, y_hat):
+        return self.mlp.loss( y, y_hat ) 
+
+    def save(self):
+        '''
+        Write parameters to disk
+        '''
+        self.mlp.save()
+        self.vae.save()
+
+    @staticmethod
+    def load(file):
+        pass
+
+
+
+class MLP(nn.ModuleDict):
+    '''
+    This learns the value of a sequence of observations. 
+    It's a custom hidden layer sized multi-layer peceptron
+    '''
+    def __init__(self, num_inputs, num_outputs, hidden_sizes, model_file_prefix):
+        '''
+        num_inputs - number of input values
+        num_outputs - should be 1 for the single value
+        hidden_sizes - list of sizes of hidden layers
+        model_file_prefix - for saving paramters. "-vae.pt" added for actual name
+        '''
+        super(MLP, self).__init__()
+
+        layers = []     # list of layers starting at input layer
         ix = 0
         prev_layer = num_inputs
         for h in hidden_sizes:
             layers.append(nn.Linear(prev_layer, h))
-            layers.append(nn.ReLU(True))
+            layers.append(nn.LeakyReLU(True))
             prev_layer = h
 
-        layers.append(nn.Linear(h, num_outputs))
-        self.seq = nn.Sequential(*layers)
+        layers.append(nn.Linear(h, num_outputs))    # add final output layer
+
+        self.seq = nn.Sequential(*layers)           # construct the model
         self.model_file = model_file_prefix + "-mlp.pt"
-        self.criterion = nn.MSELoss()
+        self.criterion = nn.MSELoss()               # simple loss - mean sqd. err
 
     def forward(self, x):
+        '''
+        Forward pass through the model. Get the value from
+        the input sequence
+        '''
         y_hat = self.seq(x)
         return y_hat
 
     def loss(self, y, y_hat):
+        '''
+        Determine loss between actual and calculated values
+        '''
         return self.criterion(y, y_hat)
 
     def save(self):
+        '''
+        Write parameters to disk
+        '''
         torch.save(self.state_dict(), self.model_file)
 
     @staticmethod
     def load(file):
-        this = Model(model_file=file)
+        '''
+        Create a new instance of an MLP with the saved parameters
+        '''
+        this = MLP(model_file=file)  # TODO - fix this error
         this.load_state_dict(torch.load(file))
         return this
 
@@ -46,7 +93,6 @@ class VAE(nn.ModuleDict):
     def __init__(self, num_inputs, ZDIMS, ZHID, model_file_prefix ):
         super(VAE, self).__init__()
 
-        # VAE section
         self.encoder = nn.Sequential(
             nn.Linear(num_inputs, ZHID),
             nn.ReLU(True)
@@ -63,11 +109,14 @@ class VAE(nn.ModuleDict):
         )
 
         self.model_file = model_file_prefix + "-vae.pt"
+
+
     def encode(self, x):
         h1 = self.encoder(x)
         mu = self.encoderMu(h1)
         log_var = self.encoderLogVar(h1)
         return mu, log_var
+
 
     def reparameterize(self, mu, log_var):
         std = torch.exp(log_var * 0.5)
